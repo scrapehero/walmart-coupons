564d093c3e40c523e18dc455f45a524f657e241f
ScrapeHero <scrapehero@users.noreply.github.com>
2018-03-14 16:54:50 +0530

    Update walmart_store_locator.py

--- walmart_store_locator.py
+++ walmart_store_locator.py
@@ -1,72 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from lxml import html  
 import csv
 import requests
+import re
 import json
 import argparse
 import traceback
 
-def locate_stores(zip_code):
+def parse(store_id):
 	"""
-	Function to locate walmart stores
+	Function to retrieve coupons in a particular walmart srore
+	:param store_id: walmart store id, you can get this id from the output of walmart store location script
+	(https://github.com/scrapehero/walmart-coupons/blob/master/walmart_store_locator.py) 
 	"""
-	url = "https://www.walmart.com/store/finder/electrode/api/stores?singleLineAddr=%s&serviceTypes=pharmacy&distance=50"%(zip_code)
-	headers = {	'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-				'accept-encoding':'gzip, deflate, br',
-				'accept-language':'en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7',
-				'cache-control':'max-age=0',
-				'upgrade-insecure-requests':'1',
-				'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
+	#sending request to get coupon related meta details
+	url = "https://www.walmart.com/store/%s/coupons"%store_id
+	headers = {"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+				"accept-encoding":"gzip, deflate, br",
+				"accept-language":"en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
+				"referer":"https://www.walmart.com/store/finder",
+				"upgrade-insecure-requests":"1",
+				"user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
 	}
-	stores = []
-	print("retrieving stores")
-	for retry in range(10):
-		try:
-			get_store = requests.get(url, headers=headers, verify=False)
-			store_response = get_store.json()
-			stores_data = store_response.get('payload',{}).get("storesData",{}).get("stores",[])
-			
-			if not stores_data:
-				print("no stores found near %s"%(zip_code))
-				return []
-			print("processing store details")
-			#iterating through all stores
-			for store in stores_data:
-				store_id = store.get('id')
-				display_name = store.get("displayName")
-				address = store.get("address").get("address")
-				postal_code = store.get("address").get("postalCode")
-				city = store.get("address").get("city")
-				phone = store.get("phone")
-				distance = store.get("distance")
 
-				data = {
-						"name":display_name,
-						"distance":distance,
-						"address":address,
-						"zip_code":postal_code,
-						"city":city,
-						"store_id":store_id,
-						"phone":phone,
+	#adding retry
+	for retry in range(5):
+		try:
+			response = requests.get(url, headers=headers)
+			raw_coupon_url_details = re.findall('"couponsData":({.*?})',response.text)
+			
+			if raw_coupon_url_details:
+				coupons_details_url_info_dict = json.loads(raw_coupon_url_details[0])
+				#these variables are used to create coupon page url
+				pid = coupons_details_url_info_dict.get('pid')
+				nid = coupons_details_url_info_dict.get('nid')
+				zid = coupons_details_url_info_dict.get('zid')
+				#coupons details are rendering from the following url
+				#example link :https://www.coupons.com/coupons/?pid=19251&nid=10&zid=vz89&storezip=20001
+				coupons_details_url = "https://www.coupons.com/coupons/?pid={0}&nid={1}&zid={2}".format(pid,nid,zid)
+				print("retrieving coupon page")
+				coupon_headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+									"Accept-Encoding":"gzip, deflate, br",
+									"Accept-Language":"en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
+									"Host":"www.coupons.com",
+									"Upgrade-Insecure-Requests":"1",
+									"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
 				}
-				stores.append(data)
-			return stores
+				response = requests.get(coupons_details_url,headers=coupon_headers)
+				coupon_raw_json = re.findall("APP_COUPONSINC\s?=\s?({.*});",response.text)
+				print("processing coupons data")
+				
+				if coupon_raw_json:
+					data = []
+					coupon_json_data =  json.loads(coupon_raw_json[0])	
+					coupons = coupon_json_data.get('contextData').get('gallery').get('podCache')
+					
+					for coupon in coupons:
+						price = coupons[coupon].get('summary')
+						product_brand = coupons[coupon].get('brand')
+						details = coupons[coupon].get('details')
+						expiration = coupons[coupon].get('expiration')
+						activated = coupons[coupon].get('activated')
+
+						wallmart_data={
+								"offer":price,
+								"brand":product_brand,
+								"description":details,
+								"activated_date":activated,
+								"expiration_date":expiration,
+								"url":coupons_details_url
+						}
+						data.append(wallmart_data)
+					return data
 		except:
-			print(trackback.format_exc())
-	
-	return []	
+			print(traceback.format_exc())
+	return []
 
 if __name__=="__main__":
-	
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument('zip_code',help = 'zip code to search')
+	argparser.add_argument('store_id',help = 'walmart store id')
 	args = argparser.parse_args()
-	zip_code = args.zip_code
-	scraped_data = locate_stores(zip_code)
+	store_id = args.store_id
+	scraped_data = parse(store_id)
 	
-	if scraped_data:
-		print ("Writing scraped data to %s_stores.csv"%(zip_code))
-		with open('%s_stores.csv'%(zip_code),'wb') as csvfile:
-		    fieldnames = ["name","store_id","distance","address","zip_code","city","phone"]
-		    writer = csv.DictWriter(csvfile,fieldnames = fieldnames,quoting=csv.QUOTE_ALL)
-		    writer.writeheader()
-		    for data in scraped_data:
-		        writer.writerow(data)
+	if scraped_data:		print ("Writing scraped data to %s_coupons.csv"%(store_id))	
+		with open('%s_coupons.csv'%(store_id),'w') as csvfile:
+			fieldnames = ["offer","brand","description","activated_date","expiration_date","url"]
+			writer = csv.DictWriter(csvfile,fieldnames = fieldnames,quoting=csv.QUOTE_ALL)
+			writer.writeheader()
+			for data in scraped_data:
+				writer.writerow(data)
